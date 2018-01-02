5866f9aca9648058281cc7a00f88fbc39145a2ca
roydaw <5164541@gmail.com>
2018-01-02 14:15:27 -0700

    Initial Commit

---
+++ Nice Dice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nice Dice
+//
+//  Created by Roy Dawson on 1/2/18.
+//  Copyright © 2018 Roy Dawson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
